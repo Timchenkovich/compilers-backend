436b71fdc8fc024a066d3b838ccddeecbe68fa65
timchenkovich <367562@niuitmo.ru>
2025-03-26 00:49:48 +0300

    Added reg info

--- llvm/lib/Target/X43/MCTargetDesc/X43MCTargetDesc.cpp
+++ llvm/lib/Target/X43/MCTargetDesc/X43MCTargetDesc.cpp
@@ -1,4 +1,24 @@
+#include "TargetInfo/X43TargetInfo.h"
+#include "llvm/MC/MCRegisterInfo.h"
+#include "llvm/MC/TargetRegistry.h"
 #include "llvm/Support/raw_ostream.h"
- 
+
+#define GET_REGINFO_ENUM
+#define GET_REGINFO_MC_DESC
+#include "X43GenRegisterInfo.inc"
+
+using namespace llvm;
+
+namespace {
+  MCRegisterInfo* createRegisterInfo(const Triple& triple) {
+    MCRegisterInfo* registerInfo = new MCRegisterInfo();
+    InitX43MCRegisterInfo(registerInfo, X43::X0);
+    return registerInfo;
+  }
+}
+
 // We need to define this function for linking succeed
-extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeX43TargetMC() {  }
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeX43TargetMC() {
+  Target& target = getTheX43Target();
+  TargetRegistry::RegisterMCRegInfo(target, createRegisterInfo);
+}
--- llvm/lib/Target/X43/X43TargetMachine.cpp
+++ llvm/lib/Target/X43/X43TargetMachine.cpp
@@ -19,4 +19,5 @@ X43TargetMachine::X43TargetMachine(const Target &T, const Triple &TT,
     : CodeGenTargetMachineImpl(
           T, "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-n32", TT, CPU, FS, Options,
           Reloc::Static, getEffectiveCodeModel(CM, CodeModel::Small), OL) {
+  initAsmInfo();
 }
